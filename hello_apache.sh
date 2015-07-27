2ca59d2ee323a7becced66d0320fa91d0c9259fe
ggallen <ggallen@users.noreply.github.com>
2015-07-27 12:56:16 -0400

    Update hello_apache.sh

--- hello_apache.sh
+++ hello_apache.sh
@@ -2,5 +2,14 @@
 
 service docker start
 
+cat << EOF >> answers.conf
+[general]
+provider = kubernetes
+
+[helloapache-app]
+image = centos/httpd # optional: choose a different image
+hostport = 80        # optional: choose a different port to expose
+EOF
+
 echo "Running projectatomic/helloapache"
 atomic run projectatomic/helloapache
