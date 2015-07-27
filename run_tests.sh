c7397fd5a2fe22d90a4e38435da79f1052280a67
ggallen <ggallen@users.noreply.github.com>
2015-07-27 13:22:17 -0400

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ echo 'Yum updating the host'
 yum -y -d0 upgrade
 
 echo 'Installing atomic'
-yum -y -d 1  install atomic
+yum -y -d 1  install atomic kubernetes
 if [ $? -eq 0 ]; then
   chmod u+x ./hello_apache.sh
   ./hello_apache.sh
