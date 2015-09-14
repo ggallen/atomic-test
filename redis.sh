2e1f9900ff54e22fea8d2e51b41582e4fa54f0e6
root <root@dhcp-41-216.bos.redhat.com>
2015-09-14 06:11:55 -0400

    Redis work.

--- redis.sh
+++ redis.sh
@@ -24,18 +24,24 @@ if [ $res -gt $ret ]; then
 fi
 
 if [ $ret -eq 0 ]; then
-#    if [ "$provider" = "kubernetes" ]; then
-#        total=0
-#        kubectl get pods | egrep -q "^\s+helloapache\s+centos/httpd\s+Running\s+[0-9]"
-#        while [ $? -ne 0 -a $total -lt 120 ]; do
-#           sleep 5
-#           total=$((total+5))
-#           kubectl get pods | egrep -q "^\s+helloapache\s+centos/httpd\s+Running\s+[0-9]"
-#        done
-#
-#        echo "Checking kubernetes pod"
-#        kubectl get pods
-#    fi
+    if [ "$provider" = "kubernetes" ]; then
+        total=0
+        kubectl get pods | egrep -q "^redis-master-.*\s+Running\s+"
+        while [ $? -ne 0 -a $total -lt 120 ]; do
+            sleep 2
+            total=$((total+2))
+            kubectl get pods | egrep -q "^redis-master-.*\s+Running\s+"
+        done
+
+        echo "Checking kubernetes pod"
+        kubectl get pods
+
+        kubectl get pods | egrep -q "^redis-master-.*\s+Running\s+"
+        res=$?
+        if [ $res -gt $ret ]; then
+            ret=$res
+        fi
+    fi
 
     sleep 3
     echo "Checking databases"
