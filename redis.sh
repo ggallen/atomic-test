fb2e4b0d26c0f7d9c888c02c1c9d15bba4048cf3
root <root@dhcp-41-200.bos.redhat.com>
2015-09-14 13:22:51 -0400

    Fix to work with kubernetes provider.

--- redis.sh
+++ redis.sh
@@ -28,7 +28,6 @@ fi
 
 if [ $ret -eq 0 ]; then
     if [ "$provider" = "docker" ]; then
-        host="0.0.0.0"
         total=0
         docker ps | egrep "redis-master" | egrep -q " Up "
         while [ $? -ne 0 -a $total -lt 120 ]; do
@@ -47,6 +46,7 @@ if [ $ret -eq 0 ]; then
             ret=$res
         fi
 
+        host="0.0.0.0"
         port=`docker ps | egrep "redis-master" | sed -e "s/.*0.0.0.0://" -e "s/->.*//"`
     elif [ "$provider" = "kubernetes" ]; then
         total=0
@@ -60,14 +60,26 @@ if [ $ret -eq 0 ]; then
         echo "Checking kubernetes pod"
         kubectl get pods
 
+        echo "Checking kubernetes services"
+        kubectl get services
+
         kubectl get pods | egrep -q "^redis-master-.*\s+Running\s+"
         res=$?
         if [ $res -gt $ret ]; then
             ret=$res
         fi
+        host=`kubectl get services | egrep "^redis-master" | awk '{print $(NF-1)}'`
+        port=`kubectl get services | egrep "^redis-master" | awk '{print $NF}' | sed -e 's%/TCP%%'`
+
     fi
 
-    sleep 3
+    times=0
+    redis-cli -h $host -p $port ping
+    while [ $? -ne 0 -a $times -lt 3 ]; do
+        total=$((times+1))
+        redis-cli -h $host -p $port ping
+    done
+
     echo "Checking connectivity to master"
     redis-cli -h $host -p $port get key
     res=$?
@@ -80,7 +92,6 @@ echo "Stopping projectatomic/redis-centos7-atomicapp"
 atomic stop projectatomic/redis-centos7-atomicapp
 res=$?
 if [ $res -gt $ret ]; then
-    echo "+++++++++++++++++++++++ FAILED ON ATOMIC STOP"
     ret=$res
 fi
 
