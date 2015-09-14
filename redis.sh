15bcdebaf907f9d1e8f11ed1ec9ff6ca21a4e0d6
root <root@dhcp-41-200.bos.redhat.com>
2015-09-14 08:24:33 -0400

    Make redis.sh work for docker provider.

--- redis.sh
+++ redis.sh
@@ -8,10 +8,13 @@ namespace = default
 provider = $provider
 
 [redismaster-app]
-hostport = 16379
+image = jasonbrooks/redis
+hostport = 6379
 
 [redisslave-app]
-master_hostport = 16379
+image = jasonbrooks/redis
+master_hostport = 6379
+hostport = 6379
 EOF
 
 ret=0
@@ -24,7 +27,28 @@ if [ $res -gt $ret ]; then
 fi
 
 if [ $ret -eq 0 ]; then
-    if [ "$provider" = "kubernetes" ]; then
+    if [ "$provider" = "docker" ]; then
+        host="0.0.0.0"
+        total=0
+        docker ps | egrep "redis-master" | egrep -q " Up "
+        while [ $? -ne 0 -a $total -lt 120 ]; do
+            sleep 2
+            total=$((total+2))
+            docker ps | egrep "redis-master" | egrep -q " Up "
+        done
+        sleep 2
+
+        echo "Checking docker containers"
+        docker ps
+
+        docker ps | egrep "redis-master" | egrep -q " Up "
+        res=$?
+        if [ $res -gt $ret ]; then
+            ret=$res
+        fi
+
+        port=`docker ps | egrep "redis-master" | sed -e "s/.*0.0.0.0://" -e "s/->.*//"`
+    elif [ "$provider" = "kubernetes" ]; then
         total=0
         kubectl get pods | egrep -q "^redis-master-.*\s+Running\s+"
         while [ $? -ne 0 -a $total -lt 120 ]; do
@@ -44,8 +68,8 @@ if [ $ret -eq 0 ]; then
     fi
 
     sleep 3
-    echo "Checking databases"
-    mysql --host 0.0.0.0 --user=username --password=password --execute="show databases;"
+    echo "Checking connectivity to master"
+    redis-cli -h $host -p $port get key
     res=$?
     if [ $res -gt $ret ]; then
         ret=$res
@@ -56,7 +80,13 @@ echo "Stopping projectatomic/redis-centos7-atomicapp"
 atomic stop projectatomic/redis-centos7-atomicapp
 res=$?
 if [ $res -gt $ret ]; then
+    echo "+++++++++++++++++++++++ FAILED ON ATOMIC STOP"
     ret=$res
+fi
+
+if [ "$provider" = "docker" ]; then
+    docker stop redis-master redis-slave
+    docker rm redis-master redis-slave
 fi
 
 exit $ret
